e8dafcf80018118033febd57991f30d8bc100849
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-11-29 20:28:13 +0530

    new template

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(CORE_DIR,'base/static'),
 ]
-
+ALLOWED_HOSTS=["*"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
