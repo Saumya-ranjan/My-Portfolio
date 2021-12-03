7e31d09396d02cb42c83a03cf84e7078d49a7256
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2021-12-03 18:59:37 +0530

    media uel

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.conf.urls import url
@@ -11,3 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
